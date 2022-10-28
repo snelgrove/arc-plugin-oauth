3baa14f562c3125d56fe75dd8dcd25a1ccac60c8
Matt Snelgrove <matt.snelgrove@gmail.com>
2022-10-28 21:56:21 +1100

    Merged import-loginhref into integration2

--- src/src/http/get-login/index.mjs
+++ src/src/http/get-login/index.mjs
@@ -1,4 +1,6 @@
 import arc from '@architect/functions'
+// eslint-disable-next-line import/no-unresolved
+import { loginHref } from 'arc-plugin-oauth'
 
 export const handler = arc.http.async(login)
 
@@ -8,14 +10,4 @@ async function login(req) {
     statusCode: 200,
     html: `<a href="${href}">Login with OAuth</a>`
   }
-}
-
-function loginHref() {
-  const redirectUrlPart = process.env.ARC_OAUTH_REDIRECT_URL
-    ? `&redirect_url=${process.env.ARC_OAUTH_REDIRECT_URL}`
-    : ''
-  if (process.env.ARC_OAUTH_USE_MOCK)
-    return 'http://localhost:3333/mock/auth/login'
-  else
-    return `https://github.com/login/oauth/authorize?client_id=${process.env.ARC_OAUTH_CLIENT_ID}${redirectUrlPart}`
 }
